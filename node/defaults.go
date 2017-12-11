56b26e24a40440a379fb48ebe445a6add53b51d6
luoie <luoiefly@163.com>
2017-12-11 18:57:24 +0800

    change main port to 58885, bootnode port to 58685

--- node/defaults.go
+++ node/defaults.go
@@ -41,7 +41,7 @@ var DefaultConfig = Config{
 	WSPort:      DefaultWSPort,
 	WSModules:   []string{"net", "web3"},
 	P2P: p2p.Config{
-		ListenAddr: ":50505",
+		ListenAddr: ":58885",
 		MaxPeers:   25,
 		NAT:        nat.Any(),
 	},
